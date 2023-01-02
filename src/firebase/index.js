bec38af2f54279549522bce0e9ddc4160ff3347d
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-02 15:40:32 +0100

    Added Authentication, Firestore and Storage instances

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,9 +12,22 @@ const firebaseConfig = {
 	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
 	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Get Authentication instance
+const auth = getAuth(app)
+
+// Get Firestore instance
+const db = getFirestore(app)
+
+// Get Storage instance
+const storage = getStorage(app)
+
 export {
     app as default,
+    auth,
+	db,
+	storage,
 }
