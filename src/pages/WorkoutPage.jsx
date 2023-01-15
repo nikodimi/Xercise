239ce6a9dd4f07d6affbec23c093638b73efd790
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-15 12:28:58 +0100

    Deleting exercises in workout now possible

--- src/pages/WorkoutPage.jsx
+++ src/pages/WorkoutPage.jsx
@@ -1,15 +1,27 @@
-import { Container, Row, Col, Button } from 'react-bootstrap'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { Container, Row, Col } from 'react-bootstrap'
+import { faArrowLeft, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate, useParams } from 'react-router-dom'
 import { useAuthContext } from '../contexts/AuthContext'
+import { useState } from 'react';
+import { doc, updateDoc, arrayRemove } from 'firebase/firestore'
+import { db } from '../firebase'
 import useGetWorkout from '../hooks/useGetWorkout'
 
 const WorkoutPage = () => {
     const { id } = useParams()
     const { currentUser } = useAuthContext()
     const { data, isLoading } = useGetWorkout(currentUser.uid, id)
+    const [edit, setEdit] = useState(false)
     const navigate = useNavigate()
+
+    const deleteExercise = async (value) => {
+        console.log("du tryckte")
+        const ref = doc(db, `users/${currentUser.uid}/workouts`, id)
+        await updateDoc(ref, {
+            exercises: arrayRemove(value)
+        })
+    }
 
     return (
         <Container className='mt-3'>
@@ -19,16 +31,22 @@ const WorkoutPage = () => {
             {!isLoading && data && (
 
             <Row>
-
                 <Col>
                     <FontAwesomeIcon size="lg" icon={faArrowLeft} onClick={() => navigate(-1)}/>
                     <div className='mt-3'>
-                        <h5>{data.title}</h5>
-                        <ul>
-                            {data.exercises?.map(exercise => (
-                                <li key={exercise.id}>{exercise.name}</li>
-                            ))}
-                        </ul>
+                        <div className='d-flex justify-content-between'>
+                            <h5>{data.title}</h5>
+                            <FontAwesomeIcon size="lg" icon={faPenToSquare} onClick={() => setEdit(!edit)} />
+                        </div>
+                        {data.exercises?.map(exercise => (
+                            <div key={exercise.id} className="d-flex justify-content-between mt-4">
+                                <h6>{exercise.name}</h6>
+                                {edit && (
+                                    <FontAwesomeIcon size="lg" icon={faTrash} onClick={() => deleteExercise(exercise)}
+                                    />
+                                )}
+                            </div>
+                        ))}
                     </div>
                 </Col>
             </Row>
