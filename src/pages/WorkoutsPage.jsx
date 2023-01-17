d1652638ed0718e8ce3e9987e8379dc394a6ac56
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-17 13:42:01 +0100

    Displaying workouts and premade workouts in tabs

--- src/pages/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage.jsx
@@ -1,7 +1,7 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap'
 import { useAuthContext } from '../contexts/AuthContext'
-import useGetUserWorkouts from '../hooks/useGetUserWorkouts'
 import { Link } from 'react-router-dom'
+import useGetUserWorkouts from '../hooks/useGetUserWorkouts'
 
 const WorkoutsPage = () => {
     const { currentUser } = useAuthContext()
@@ -10,25 +10,47 @@ const WorkoutsPage = () => {
     return (
 
         <Container>
-
-            {isLoading && !data && (<p>Loading plz wait...</p>)}
-
-            {!isLoading && data && (
-                <Row>
-                    <Col xs={12}>
-                        <h4 className='my-3'>
-                            My Exercises
-                        </h4>
-                        {data.map(workout => (
-                            <div key={workout.id}>
-                                <Link to={`/workouts/${workout.id}`}><h5>{workout.title}</h5></Link>
-                                <p>{workout.exercises.length} exercises</p>
+            <Row>
+                <Col xs={12}>
+                    <Tabs
+                        defaultActiveKey="my workouts"
+                        id="uncontrolled-tab-example"
+                        className="mb-3"
+                    >
+                        <Tab eventKey="my workouts" title="My Workouts">
+                        
+                        {isLoading && !data && (<p>Loading plz wait...</p>)}
+                        
+                        {!isLoading && data && (
+                            <div>
+                                {data.filter(d => d.premade != "yes").map(workout => (
+                                    <div key={workout.id} className="d-flex justify-content-between workout-box mt-2">
+                                        <Link to={`/workouts/${workout.id}`}><h5>{workout.title}</h5></Link>
+                                        <p>{workout.exercises.length} exercises</p>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </Col>
-                </Row>
-            )}                
+                        )}
+                        </Tab>
 
+                        <Tab eventKey="premade workouts" title="Premade Workouts">
+                        
+                        {isLoading && !data && (<p>Loading plz wait...</p>)}
+                        
+                        {!isLoading && data && (
+                            <div>
+                                {data.filter(d => d.premade == "yes").map(workout => (
+                                    <div key={workout.id} className="d-flex justify-content-between workout-box mt-2">
+                                        <Link to={`/workouts/${workout.id}`}><h5>{workout.title}</h5></Link>
+                                        <p>{workout.exercises.length} exercises</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                        </Tab>
+                    </Tabs>         
+                </Col>
+            </Row>
         </Container>
     )
 }
