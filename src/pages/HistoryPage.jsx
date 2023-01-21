c7528ff9dc70e8d748dbc076ca2550e306bef7e3
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-21 11:34:17 +0100

    Fixed layout and styling on the history page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -14,7 +14,7 @@ const HistoryPage = () => {
     console.log('data', data)
 
     return (
-        <Container className="d-flex justify-content-center align-items-center flex-column">
+        <Container className="d-flex align-items-center flex-column">
             
             <Row className='mt-4'>
                 <Col xs={12}>
@@ -30,18 +30,31 @@ const HistoryPage = () => {
                         
             {!isLoading && data && (
                 
-                <Row>
+                <Row className='w-100'>
                     <Col xs={12}>
-                        <h4>Latest workouts</h4>
+                        <div className='workout-wrapper'>
+                            <div className=''>
+                                <h4>Latest workouts</h4>
+                            </div>
+
                             {data.filter(d => d.completed.length > 0).map((workout,i) => (
-                                
-                                <div key={i}>
-                                    <h5>{workout.title}</h5>
-                                    
-                                    <p>{moment(workout.completed[0]?.toMillis()).format('YYYY-MM-DD')}</p>
+                                <div key={i} className="workout-content mt-3">
+                                    <div className="d-flex justify-content-between">
+                                        <h5>{workout.title}</h5>
+                                        <p>{moment(workout.completed[0]?.toMillis()).format('YYYY-MM-DD')}</p>
+                                    </div>
+                                    <div className='mt-2'>
+                                        {workout.exercises.map((exercise,i) => (
+                                            <div key={i} className="d-flex justify-content-between">
+                                                <p>{exercise.sets.length} x </p>
+                                                <p>{exercise.name}</p>
+                                            </div>
+                                        ))}
+
+                                    </div>
                                 </div>
                             ))}
-
+                        </div>
                     </Col>
                 </Row>
             )}
