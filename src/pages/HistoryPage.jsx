a10735875fb784114a53974e6aa7dff34358adc3
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-22 12:20:44 +0100

    Created a history list component

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -3,8 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { useAuthContext } from '../contexts/AuthContext'
 import Calendar from 'react-calendar';
 import useGetUserWorkouts from '../hooks/useGetUserWorkouts'
-import moment from 'moment'
 import 'react-calendar/dist/Calendar.css';
+import HistoryList from '../components/HistoryList';
 
 const HistoryPage = () => {
     const { currentUser } = useAuthContext()
@@ -30,33 +30,7 @@ const HistoryPage = () => {
                         
             {!isLoading && data && (
                 
-                <Row className='w-100'>
-                    <Col xs={12}>
-                        <div className='workout-wrapper'>
-                            <div className=''>
-                                <h4>Latest workouts</h4>
-                            </div>
-
-                            {data.filter(d => d.completed.length > 0).map((workout,i) => (
-                                <div key={i} className="workout-content mt-3">
-                                    <div className="d-flex justify-content-between">
-                                        <h5>{workout.title}</h5>
-                                        <p>{moment(workout.completed[0]?.toMillis()).format('YYYY-MM-DD')}</p>
-                                    </div>
-                                    <div className='mt-2'>
-                                        {workout.exercises.map((exercise,i) => (
-                                            <div key={i} className="d-flex justify-content-between">
-                                                <p>{exercise.sets.length} x </p>
-                                                <p>{exercise.name}</p>
-                                            </div>
-                                        ))}
-
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </Col>
-                </Row>
+                <HistoryList data={data} />
             )}
 
         </Container>
