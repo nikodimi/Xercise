46d3981c1e4e4588ff2a0b64cd8669b09563dde8
nddesigns <56118523+nikodimi@users.noreply.github.com>
2023-01-22 23:16:05 +0100

    Checking if changes to menu and hook fixes bug on netlify

--- src/components/MuscleMenu.jsx
+++ src/components/MuscleMenu.jsx
@@ -1,18 +1,31 @@
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import useGetMuscles from '../hooks/useGetMuscles'
 
-const MuscleMenu = ({data}) => {
+const MuscleMenu = () => {
+    const {data, isLoading} = useGetMuscles()
 
     return (
-        <Row className="scrollbar">
-            {data.map(muscle => (
-                <Col xs={4} className="scrollbar-item" key={muscle.id} >
-                    <Link to={`/muscles/${muscle.name}`}>
-                        <h5>{muscle.id}</h5>
-                    </Link>
-                </Col>
-            ))}
-        </Row>
+        
+        <>
+            {isLoading && !data && (<p>Loading data...</p>)} 
+
+            {!isLoading && data && (
+            
+            <Row className="scrollbar">
+                {data.map(muscle => (
+                    <Col xs={4} className="scrollbar-item" key={muscle.id} >
+                        <Link to={`/muscles/${muscle.name}`}>
+                            <h5>{muscle.id}</h5>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+
+            )}
+        </>
+        
+
     )
 }
 
--- src/hooks/useGetExercisesByMuscle.js
+++ src/hooks/useGetExercisesByMuscle.js
@@ -1,19 +1,25 @@
 import { db } from '../firebase'
 import { collection, query } from 'firebase/firestore'
 import { useFirestoreQueryData } from '@react-query-firebase/firestore'
+import useGetCollection from "./useGetCollection";
 
-const useGetExercisesByMuscle = (muscle) => {
-    // Get reference of collection 'exercises'
-    const queryRef = query(
-		collection(db, `muscles/${muscle}/exercises`)
-	)
+const useGetExercisesByMuscle = (value) => {
+    return useGetCollection('muscles', value)
 
-	const exercisesByMuscleQuery = useFirestoreQueryData([`${muscle}-exercises`], queryRef, {
-		idField: 'id',
-		subscribe: 'true' 
-	})
 
-    return exercisesByMuscleQuery
+// const useGetExercisesByMuscle = (muscle) => {
+//     // Get reference of collection 'exercises'
+//     const queryRef = query(
+// 		collection(db, `muscles/${muscle}/exercises`)
+// 	)
+
+// 	const exercisesByMuscleQuery = useFirestoreQueryData([`${muscle}-exercises`], queryRef, {
+// 		idField: 'id',
+// 		subscribe: 'true' 
+// 	})
+
+//     return exercisesByMuscleQuery
+// }
 }
 
 export default useGetExercisesByMuscle
